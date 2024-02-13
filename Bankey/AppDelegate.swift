977935998ef7051696fd616b8e723011710defe2
chryshel fonseka <chryshelfonseka@Chryshels-MacBook-Pro.local>
2024-02-13 19:00:30 +0530

    removing storyboards files and adding a custom view conteroller

--- Bankey/AppDelegate.swift
+++ Bankey/AppDelegate.swift
@@ -9,28 +9,21 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    // in our AppDelegate we directly getting refference to our window
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        //making it key and visible
+        window?.makeKeyAndVisible()
+        //setting a background color
+        window?.backgroundColor = .orange
+        //setting the ViewController as the rootview controller in our AppDelegate
+        window?.rootViewController = ViewController()
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
